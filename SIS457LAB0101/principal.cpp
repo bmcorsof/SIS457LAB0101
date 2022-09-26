885a110271b23a307b2be054d74edada1dc5591e
UnseR <UnseR@DESKTOP-0GM8FU2>
2022-09-26 16:31:04 -0400

    Agregar archivos de proyecto.

---
+++ SIS457LAB0101/principal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Nombre: Burlecot Matias Corso Flores" << endl;
+	cout << "Edad: 20 " << endl;
+	cout << "CU: 35-5086" << endl;
+	cout << "CI: 10410335" << endl;
+	return 0;
+}
